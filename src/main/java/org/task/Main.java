577addc36cada36246976f8e1f10726fa3873c26
Illia Budaiev <kurin899@gmail.com>
2023-02-24 19:29:59 +0200

    PE01- First Search Algorithm done with ConsoleService

--- src/main/java/org/task/Main.java
+++ src/main/java/org/task/Main.java
@@ -2,7 +2,9 @@ package org.task;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        FileSearcher.search("C:\\Users\\kurin\\IdeaProjects\\ProductEngine\\",12,"text");
+        System.out.println("Starting");
+        ConsoleService.start();
+        System.out.println("Complete");
     }
+
 }
